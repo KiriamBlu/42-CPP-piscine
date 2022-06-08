56eb564658dd50518f981f669fc6eb0b3f9ce2af
Joan Sanfelipe <88371986+KiriamBlu@users.noreply.github.com>
2022-06-08 15:32:26 +0200

    Update main.cpp

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -1,14 +1,8 @@
 #include "contact_var.hpp"
 #include "phonebook.hpp"
 
-void lekas()
-{
-	system("leaks phonebook");
-}
-
 int	main(void)
 {
-	atexit(lekas);
 	phonebook phbook;
 
 	phbook.phonebook_start();
