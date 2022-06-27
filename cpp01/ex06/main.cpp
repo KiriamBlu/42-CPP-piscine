4fc85a06f12df568563d841f6bd43cc0281982e5
Joan <Kiriamblu@gmail.com>
2022-06-27 15:15:25 +0200

    finished cpp01

--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char const *argv[])
 {
 	if (argc != 2)
-		std::cout << "More arguments than needed" << std::endl;
+		std::cout << "Arguments cuality failed" << std::endl;
 	else
 		karenfilter(argv[1]);
 	return 0;
