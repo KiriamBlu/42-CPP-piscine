e409f665504c41c0f51c41a835249234d892f287
KiriamBlu <Kiriamblu@gmail.com>
2023-06-15 15:30:51 +0200

    added bigger main ex00

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -20,5 +20,23 @@ int main( void ) {
     std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
     std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 
+    float l = 3.3;
+    float m = 4.4;
+
+    ::swap(l, m);
+
+    std::cout << "l = " << l << " , m = " << m << std::endl;
+    std::cout << "min( l, d ) = " << ::min( l, m ) << std::endl;
+    std::cout << "max( l, d ) = " << ::max( l, m ) << std::endl;
+
+    void *var1 = &l;
+    void *var2 = &m;
+
+    ::swap(var1, var2);
+
+    std::cout << "var1 = " << var1 << " , var2 = " << var2 << std::endl;
+    std::cout << "min( var1, var2 ) = " << ::min( var1, var2 ) << std::endl;
+    std::cout << "max( var1, var2 ) = " << ::max( var1, var2 ) << std::endl;
+
     return 0;
 }
